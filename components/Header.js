79ceaa37f8ecc340d59df2d8abd29ca3ddfd6d12
rachid-webdev <rasheedolamide2015@gmail.com>
2021-11-16 21:58:39 +0100

    implement session with Header components

--- components/Header.js
+++ components/Header.js
@@ -7,12 +7,12 @@ import {
      StarIcon,
 } from "@heroicons/react/solid";
 
-import { signIn } from "next-auth/client";
-// Google client Id
-// 112149833318-1tlbd7s48fk7e247r6pa0e7h1gu5sfer.apps.googleusercontent.com
-//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
+import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 
 const Header = () => {
+     const [session] = useSession();
+     const router = useRouter();
      return (
           <div className="sticky bg-[#040714] top-0 z-[1000] flex h-[72px] items-center px-10 md:px-12">
                <Image
@@ -20,47 +20,59 @@ const Header = () => {
                     src="/images/logo.svg"
                     width={80}
                     height={80}
+                    onClick={() => router.push("/")}
                />
-               <div className="hidden ml-10 md:flex items-center space-x-6">
-                    <a className="header-link group">
-                         <HomeIcon className="h-4" />
-                         <span className="span">Home</span>
-                    </a>
-                    <a className="header-link group">
-                         <SearchIcon className="h-4" />
-                         <span className="span">Search</span>
-                    </a>
-                    <a className="header-link group">
-                         <PlusIcon className="h-4" />
-                         <span className="span">Watchlist</span>
-                    </a>
-                    <a className="header-link group">
-                         <StarIcon className="h-4" />
-                         <span className="span">Originals</span>
-                    </a>
-                    <a className="header-link group">
-                         <img
-                              src="/images/movie-icon.svg"
-                              alt=""
-                              className="h-5"
-                         />
-                         <span className="span">Movies</span>
-                    </a>
-                    <a className="header-link group">
-                         <img
-                              src="/images/series-icon.svg"
-                              alt=""
-                              className="h-5"
-                         />
-                         <span className="span">Series</span>
-                    </a>
-               </div>
-               <button
-                    className="ml-auto uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200"
-                    onClick={signIn}
-               >
-                    Login
-               </button>
+               {session && (
+                    <div className="hidden ml-10 md:flex items-center space-x-6">
+                         <a className="header-link group">
+                              <HomeIcon className="h-4" />
+                              <span className="span">Home</span>
+                         </a>
+                         <a className="header-link group">
+                              <SearchIcon className="h-4" />
+                              <span className="span">Search</span>
+                         </a>
+                         <a className="header-link group">
+                              <PlusIcon className="h-4" />
+                              <span className="span">Watchlist</span>
+                         </a>
+                         <a className="header-link group">
+                              <StarIcon className="h-4" />
+                              <span className="span">Originals</span>
+                         </a>
+                         <a className="header-link group">
+                              <img
+                                   src="/images/movie-icon.svg"
+                                   alt=""
+                                   className="h-5"
+                              />
+                              <span className="span">Movies</span>
+                         </a>
+                         <a className="header-link group">
+                              <img
+                                   src="/images/series-icon.svg"
+                                   alt=""
+                                   className="h-5"
+                              />
+                              <span className="span">Series</span>
+                         </a>
+                    </div>
+               )}
+               {!session ? (
+                    <button
+                         className="ml-auto uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200"
+                         onClick={signIn}
+                    >
+                         Login
+                    </button>
+               ) : (
+                    <img
+                         src={session.user.image}
+                         alt=""
+                         className=" ml-auto h-12 w-12 rounded-full  object-cover cursor-pointer"
+                         onClick={signOut}
+                    />
+               )}
           </div>
      );
 };
