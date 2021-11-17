046d98f8ad8a06dd379a1f255473b901083259d3
rachid-webdev <rasheedolamide2015@gmail.com>
2021-11-17 18:26:33 +0100

    ShowsCollectionsm Components

---
+++ components/ShowsCollections.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShowsCollection = () => {
+     return (
+          <div>
+               ShowsCollection
+               <p>import</p>
+          </div>
+     );
+};
+
+export default ShowsCollection;
