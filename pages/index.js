73618fa74b68e535c5d7374817a4ed7624a8b3fd
rachid-webdev <rasheedolamide2015@gmail.com>
2021-11-18 22:43:05 +0100

    add meta tag

--- pages/index.js
+++ pages/index.js
@@ -8,80 +8,93 @@ import Slider from "../components/Slider";
 import ShowsCollection from "../components/ShowsCollection";
 
 export default function Home({
-  popularMovies,
-  popularShows,
-  top_ratedMovies,
-  top_ratedShows,
+     popularMovies,
+     popularShows,
+     top_ratedMovies,
+     top_ratedShows,
 }) {
-  const [session] = useSession();
+     const [session] = useSession();
 
-  return (
-    <div>
-      <Head>
-        <title>
-          Disney+ | The streaming home of Disney, Pixar, Marvel, Star Wars, Nat
-          Geo and Star
-        </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      {!session ? (
-        <Hero />
-      ) : (
-        <main className="relative min-h-screen after:bg-home after:bg-center after:bg-cover after:bg-no-repeat after:bg-fixed after:absolute after:inset-0 after:z-[-1]">
-          <Slider />
-          <Brands />
-          <MoviesCollection results={popularMovies} title="Popular Movies" />
-          <ShowsCollection results={popularShows} title="Popular Shows" />
+     return (
+          <div>
+               <Head>
+                    <title>
+                         Disney+ | The streaming home of Disney, Pixar, Marvel,
+                         Star Wars, Nat Geo and Star
+                    </title>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta
+                         name="google-site-verification"
+                         content="s2mMxJ0GYBBg05vnYpjKfpXEELxHYcwXKf7AsoE_pHk"
+                    />
+               </Head>
+               <Header />
+               {!session ? (
+                    <Hero />
+               ) : (
+                    <main className="relative min-h-screen after:bg-home after:bg-center after:bg-cover after:bg-no-repeat after:bg-fixed after:absolute after:inset-0 after:z-[-1]">
+                         <Slider />
+                         <Brands />
+                         <MoviesCollection
+                              results={popularMovies}
+                              title="Popular Movies"
+                         />
+                         <ShowsCollection
+                              results={popularShows}
+                              title="Popular Shows"
+                         />
 
-          <MoviesCollection
-            results={top_ratedMovies}
-            title="Top Rated Movies"
-          />
-          <ShowsCollection results={top_ratedShows} title="Top Rated Shows" />
-        </main>
-      )}
-    </div>
-  );
+                         <MoviesCollection
+                              results={top_ratedMovies}
+                              title="Top Rated Movies"
+                         />
+                         <ShowsCollection
+                              results={top_ratedShows}
+                              title="Top Rated Shows"
+                         />
+                    </main>
+               )}
+          </div>
+     );
 }
 
 export async function getServerSideProps(context) {
-  const session = await getSession(context);
+     const session = await getSession(context);
 
-  const [
-    popularMoviesRes,
-    popularShowsRes,
-    top_ratedMoviesRes,
-    top_ratedShowsRes,
-  ] = await Promise.all([
-    fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
-    ),
-    fetch(
-      `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
-    ),
-    fetch(
-      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
-    ),
-    fetch(
-      `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
-    ),
-  ]);
-  const [popularMovies, popularShows, top_ratedMovies, top_ratedShows] =
-    await Promise.all([
-      popularMoviesRes.json(),
-      popularShowsRes.json(),
-      top_ratedMoviesRes.json(),
-      top_ratedShowsRes.json(),
-    ]);
+     const [
+          popularMoviesRes,
+          popularShowsRes,
+          top_ratedMoviesRes,
+          top_ratedShowsRes,
+     ] = await Promise.all([
+          fetch(
+               `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+          ),
+          fetch(
+               `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+          ),
+          fetch(
+               `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
+          ),
+          fetch(
+               `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
+          ),
+     ]);
+     const [popularMovies, popularShows, top_ratedMovies, top_ratedShows] =
+          await Promise.all([
+               popularMoviesRes.json(),
+               popularShowsRes.json(),
+               top_ratedMoviesRes.json(),
+               top_ratedShowsRes.json(),
+          ]);
 
-  return {
-    props: {
-      session,
-      popularMovies: popularMovies.results,
-      popularShows: popularShows.results,
-      top_ratedMovies: top_ratedMovies.results,
-      top_ratedShows: top_ratedShows.results,
-    },
-  };
+     return {
+          props: {
+               session,
+               popularMovies: popularMovies.results,
+               popularShows: popularShows.results,
+               top_ratedMovies: top_ratedMovies.results,
+               top_ratedShows: top_ratedShows.results,
+          },
+     };
 }
